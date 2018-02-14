08b0b96f5e1c7d1e4f62309577f6a734a16debc3
Javier Pimás <elpochodelagente@gmail.com>
2018-02-14 01:03:07 -0300

    remove stuff from computer that belongs to cognos instead of nopsys

--- src/types.h
+++ src/types.h
@@ -25,33 +25,13 @@ typedef struct display_info_t
 
 } display_info_t;
 
-typedef struct readonly_page_t
-{
-	unsigned long virtualAddress;
-	unsigned long physicalAddress;
-	unsigned char contents[4096];
-} readonly_page_t;
-
-typedef struct snapshot_info_t
-{
-	unsigned long pagesSaved;
-	unsigned long pagesToSave;
-	readonly_page_t *pages;
-} snapshot_info_t;
 
 typedef struct computer_t
 {
-	snapshot_info_t snapshot;
 	display_info_t video_info;
 	multiboot_info_t *mbi;
 	void *image;
 	uint32_t image_length;
-	void *snapshot_start, *snapshot_end;
-	void (*page_fault_handler)(void*);
-	bool in_gc;
-	int  in_page_fault;
-	void *page_fault_address;
-	uint32_t total_page_faults;
 } computer_t;
 
 

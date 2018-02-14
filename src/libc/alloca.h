dc8d64b4f3f196a7cd2f9bb3cf7244bbf8a5acfe
Javier Pimás <elpochodelagente@gmail.com>
2018-02-14 01:02:21 -0300

    fix printf args, also output to console stdout, add a breakpoint on perror, add alloca builtin

--- src/libc/alloca.h
+++ src/libc/alloca.h
@@ -2,7 +2,13 @@
 #ifndef _ALLOCA_H_
 #define _ALLOCA_H_
 
-// stub
+void *alloca(size_t size);
+
+#ifdef __GNUC__
+#define alloca __builtin_alloca
+#else
+#error dont have alloca for this compiler
+#endif
 
 #endif  // _ALLOCA_H_
 

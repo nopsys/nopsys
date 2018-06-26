e7f476b422288450f466582654dc52591acc9ec3
Javier Pimás <elpochodelagente@gmail.com>
2018-06-26 01:26:48 -0300

    typos

--- src/ints-asm.asm
+++ src/ints-asm.asm
@@ -243,11 +243,11 @@ get_tsc:
 
 enable_sse:
 	mov rax, cr0
-	and rax, ~0x4		;clear coprocessor emulation CR0.EM
-	or  rax, 0x2			;set coprocessor monitoring  CR0.MP
+	and rax, ~0x4		; clear coprocessor emulation CR0.EM
+	or  rax, 0x2		; set coprocessor monitoring  CR0.MP
 	mov cr0, rax
 	mov rax, cr4
-	or  rax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+	or  rax, 3 << 9		; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
 	mov cr4, rax
 	ret
 
